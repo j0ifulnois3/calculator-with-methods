5e53a48b668f5bd9caf4ceed7176e7a963c41902
Joi McBrayer <j0ifulnois3@gmail.com>
2026-04-13 13:34:54 -0500

    Feat: Menu and Format method

---
+++ src/main/java/com/pluralsight/Format.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+
+public class Format {
+    public static void main(String[] args) {
+        formatName("Bob","Smith");
+
+    }
+
+    private static void formatName(String first, String last) {
+        System.out.println(last + ", " + first);
+    }
+
+
+}
