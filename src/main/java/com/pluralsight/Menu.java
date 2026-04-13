ce8b56ca6b133219b86bae1dbac3eca8f00f2f1a
Joi McBrayer <j0ifulnois3@gmail.com>
2026-04-13 13:24:19 -0500

    Feat: Menu method

---
+++ src/main/java/com/pluralsight/Menu.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+public class Menu {
+    public static void main(String[] args) {
+        displayMenu();
+    }
+
+    private static void displayMenu() {
+        System.out.println("Display Menu: 1) Coffee = $3.99" + "2) Tea = $2.99 3) Cookie = $1.99");
+    };
+}
