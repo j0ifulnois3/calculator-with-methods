f4b6c33e17be6b8e82864d200e6c36cc0223cc69
Joi McBrayer <j0ifulnois3@gmail.com>
2026-04-13 13:04:43 -0500

    Feat: Initial Method

---
+++ src/main/java/com/pluralsight/hello.java
@@ -0,0 +1,28 @@
+package com.pluralsight;
+
+public class hello {
+    public static void main(String[] args) {
+        sayGoodmorning(); //"Good Morning!"
+        sayHello(); //prints "Hello, World!"
+        sayGoodbye(); //prints "Goodbye!"
+
+
+
+    }
+
+    private static void sayGoodmorning() {
+        System.out.println("GoodMorning");
+    }
+
+    private static void sayHello() {
+        System.out.println("Hello, World!");
+
+
+    }
+
+    private static void sayGoodbye() {
+        System.out.println("Goodbye");
+    }
+
+
+}
